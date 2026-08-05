4e0386e56134084057a5440afd92195b9daa010e
Summit Vishwas <soyumsogood@gmail.com>
2026-08-05 07:15:01 +0530

    Fix navigation config filename

---
+++ src/config/navigation.ts
@@ -0,0 +1,30 @@
+export const navigation = [
+  {
+    title: "Company",
+    href: "/company",
+  },
+  {
+    title: "Ecosystem",
+    href: "/ecosystem",
+  },
+  {
+    title: "Brands",
+    href: "/brands",
+  },
+  {
+    title: "Technology",
+    href: "/technology",
+  },
+  {
+    title: "Careers",
+    href: "/careers",
+  },
+  {
+    title: "Partner",
+    href: "/partner",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+];
